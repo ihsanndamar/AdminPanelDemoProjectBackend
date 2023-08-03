0bc07e6b6206b345922d7b18c50fd70cdcdea82e
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2023-08-03 09:43:08 +0300

    feat added controller

---
+++ src/Presentation/WebAPI/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        #region CRUD Operations
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            
+        }
+
+
+
+
+        #endregion
+    }
+}
