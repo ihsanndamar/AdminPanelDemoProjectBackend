201d3111d396e203bb0b9116c17ed1138285ea40
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2023-08-05 01:34:39 +0300

    feat: added bulding entity

---
+++ src/Core/Domain/Entities/Building.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+
+namespace Domain.Entities
+{   
+    [DynamoDBTable("Buildings")]
+    public class Building
+    {
+    
+            [DynamoDBHashKey("id")]
+            public Guid Id { get; set; }
+            [DynamoDBProperty("BuildingType")]
+            public string BuildingType { get; set; }
+            [DynamoDBProperty("BuildingCost")]
+            public long BuildingCost { get; set; }
+            [DynamoDBProperty("BuildingArea")]
+            public int ConstructionTime { get; set; }
+
+    
+    }
+}
