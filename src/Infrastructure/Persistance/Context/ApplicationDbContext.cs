1ca9b04c65b46314be8255084b8e37d19259aea1
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2023-08-13 20:26:30 +0300

    fix: new conn string

--- src/Infrastructure/Persistance/Context/ApplicationDbContext.cs
+++ src/Infrastructure/Persistance/Context/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace Persistance.Context
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-            optionsBuilder.UseMySQL("Server=demeserver.mysql.database.azure.com;Port=3306;Database=paneldb;Username=ihsan;Password=password!1;");
+            optionsBuilder.UseMySQL("Server=paneldb.czhqy4et8tob.eu-north-1.rds.amazonaws.com;Port=3306;Database=paneldb;Username=ihsan;Password=password!1;");
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
