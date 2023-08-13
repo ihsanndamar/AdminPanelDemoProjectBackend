bb6eaedf929d43ab2d13067120c662d8c191a324
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2023-08-13 22:26:17 +0300

    fix last con  string

--- src/Infrastructure/Persistance/Context/ApplicationDbContext.cs
+++ src/Infrastructure/Persistance/Context/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace Persistance.Context
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-            optionsBuilder.UseMySQL("Server=adminpanel.czhqy4et8tob.eu-north-1.rds.amazonaws.com;Port=3306;Username=admin;Password=password!1;");
+            optionsBuilder.UseMySQL("Server=test.czhqy4et8tob.eu-north-1.rds.amazonaws.com;Port=3306;Username=test;Password=1029384756;Database=paneldb;");
         
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
